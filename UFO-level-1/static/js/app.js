a8fe1cb0231e6bb1c1426aa8ca77079b9f6695b9
Stefany Lima <limastefany900@gmail.com>
2021-10-04 02:50:41 -0500

    add instructions to app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,17 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// YOUR CODE HERE! LEVEL 1
+
+/* Using the UFO dataset provided in the form of an array of JavaScript objects, 
+write code that appends a table to your web page and 
+then adds new rows of data for each UFO sighting */
+
+
+
+/* Have a column for date/time, city, state, country, shape, and comment */
+
+
+
+/* Use a date form in your HTML document and write JavaScript code that will 
+listen for events and search through the date/time column to find rows that match user input */
